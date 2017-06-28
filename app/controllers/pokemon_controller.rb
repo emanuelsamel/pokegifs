e4cce69708216bfd0ea07cd2b33ee0f3f52c2606
emanuelsamel <mannys94@hotmail.com>
2017-06-28 17:04:35 -0400

    poke

---
+++ app/controllers/pokemon_controller.rb
@@ -0,0 +1,15 @@
+class PokemonController < ApplicationController
+
+  def index
+    res = Typhoeus.get(res = Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=YOUR_KEY_HERE&q=pikachu&rating=g", followlocation: true)
+
+    body = JSON.parse(res.body)
+    render json: {
+    name: body["name"],
+    id: body["id"],
+    types: body["types"]
+
+
+}
+  end
+end
