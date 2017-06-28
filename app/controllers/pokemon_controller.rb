14ea7bf3e99dd5e62eca09fa3c25b05bc7d67f30
emanuelsamel <mannys94@hotmail.com>
2017-06-28 17:59:02 -0400

    poke+gif

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,16 +1,20 @@
 class PokemonController < ApplicationController
 
-  def index
-    res = Typhoeus.get(res = Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=YOUR_KEY_HERE&q=pikachu&rating=g", followlocation: true)
+ def show
+    poke_api_response = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/", followlocation: true)
+    poke_body = JSON.parse(poke_api_response.body)
 
-    body = JSON.parse(res.body)
-    render json: {
-    name: body["name"],
-    id: body["id"],
-    types: body["types"].map do |type|
-      type["type"]["name"]
-end
+    giphy = Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_KEY"]}&q=#{poke_body["name"]}&rating=g", followlocation: true)
+    giphy_body = JSON.parse(giphy.body)
 
-}
+   render json: {
+      id: poke_body["id"],
+      name: poke_body["name"],
+      types: poke_body["types"].map do |type|
+        type["type"]["name"]
+      end,
+      gif: giphy_body["data"].first["images"]["original"]["url"]
+    }
   end
+
 end
